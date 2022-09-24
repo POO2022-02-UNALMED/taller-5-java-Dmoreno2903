df979835646c3f28f27fc4612ec17a66a4a1f359
Juan Diego Aguirre Moreno <jaguirremo@unal.edu.co>
2022-09-24 16:31:13 -0500

    Test

--- src/main/java/zooAnimales/Anfibio.java
+++ src/main/java/zooAnimales/Anfibio.java
@@ -27,7 +27,7 @@ public class Anfibio extends Animal{
 		Animal.setTotalAnimales();
 		return rana;
 	}
-	public static Anfibio crearSalamandras(String nombre, int edad, String genero) {
+	public static Anfibio crearSalamandra(String nombre, int edad, String genero) {
 		Anfibio salamandra = new Anfibio(nombre, edad, "selva", genero, "negro y amarillo", false);
 		Anfibio.listado.add(salamandra);
 		Anfibio.salamandras++;
--- src/main/java/zooAnimales/Mamifero.java
+++ src/main/java/zooAnimales/Mamifero.java
@@ -27,7 +27,7 @@ public class Mamifero extends Animal{
 		listado.add(caballo);
 		return caballo;
 	}
-	public static Mamifero crearLeones(String nombre, int edad, String genero) {
+	public static Mamifero crearLeon(String nombre, int edad, String genero) {
 		Mamifero leon = new Mamifero(nombre, edad, "selva", genero,true, 4);
 		Mamifero.listado.add(leon);
 		leones++;
